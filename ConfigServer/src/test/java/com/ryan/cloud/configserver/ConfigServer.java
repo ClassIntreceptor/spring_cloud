417e987f17fd9fdc98b284c668015410036911be
13966736115@163.com <wps19950910>
2019-01-13 18:17:52 +0800

    修改配置中心，增加外部配置文件

--- ConfigServer/src/test/java/com/ryan/cloud/configserver/ConfigServer.java
+++ ConfigServer/src/test/java/com/ryan/cloud/configserver/ConfigServer.java
@@ -1,4 +1,4 @@
-package com.ryan.cloud.configserver38001;
+package com.ryan.cloud.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Configserver38001ApplicationTests {
+public class ConfigServer {
 
     @Test
     public void contextLoads() {
