3cd2c0a70f0122a02d6c9b6ab87daabcad36dd64
13966736115@163.com <wps19950910>
2019-01-16 22:35:10 +0800

    配置注册中心

--- RegistryServer/src/test/java/com/ryan/cloud/registryserver/RegistryServerApplicationTests.java
+++ RegistryServer/src/test/java/com/ryan/cloud/registryserver/RegistryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ryan.cloud.registryserver37001;
+package com.ryan.cloud.registryserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class Registryserver37001ApplicationTests {
+@SpringBootTest(classes = RegistryServerApplicationTests.class)
+public class RegistryServerApplicationTests {
 
     @Test
     public void contextLoads() {
